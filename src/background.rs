8858b3a5b128b55fc1cde08c28954fba2e642dcc
João Koritar <jakoritarleite@gmail.com>
2024-09-03 20:33:31 -0300

    Improve Background buffer resizing

--- src/background.rs
+++ src/background.rs
@@ -16,60 +16,53 @@ use crate::render::Drawable;
 pub enum Background {
     Image {
         image: DynamicImage,
-        original: Option<DynamicImage>,
+        resized_image_buffer: Vec<u8>,
     },
     Color(Color),
 }
 
 impl Background {
     pub fn resize(&mut self, size: PhysicalSize<u32>) {
-        // TODO refactor this piece of shit
-        if let Self::Image { image, original } = self {
-            let img = match original {
-                Some(original) => original,
-                None => image,
-            };
+        let Self::Image {
+            image,
+            resized_image_buffer,
+        } = self
+        else {
+            return;
+        };
 
-            let resized = img.resize_to_fill(
-                size.width,
-                size.height,
-                image::imageops::FilterType::Nearest,
-            );
+        let resized = image.resize_to_fill(
+            size.width,
+            size.height,
+            image::imageops::FilterType::Lanczos3,
+        );
 
-            if original.is_none() {
-                *original = Some(image.clone());
-            }
+        // The image contains WxH pixels, where each pixel is 32-bit long
+        let mut rgba_data = Vec::with_capacity((resized.width() * resized.height() * 4) as usize);
 
-            *image = resized;
+        for (_, _, pixel) in resized.pixels() {
+            let [red, green, blue, alpha] = pixel.0;
+
+            rgba_data.push(red);
+            rgba_data.push(green);
+            rgba_data.push(blue);
+            rgba_data.push(alpha);
         }
+
+        *resized_image_buffer = rgba_data;
     }
 }
 
 impl Drawable for Background {
     fn draw(&mut self, _window: &Window, buffer: &mut Pixmap) -> Result<(), DrawError> {
         match self {
-            Background::Image { image, original: _ } => {
-                // TODO: instead of keeping both original and image instances, keep only the
-                // original one and save the rgba_data (resized_image_buffer), so we don't need to
-                // do this processing every time.
-                let inner_buf = buffer.data_mut();
-
-                // The image contains WxH pixels, where each pixel is 32-bit long
-                let mut rgba_data =
-                    Vec::with_capacity((image.width() * image.height() * 4) as usize);
-
-                for (_, _, pixel) in image.pixels() {
-                    let [red, green, blue, alpha] = pixel.0;
-
-                    rgba_data.push(red);
-                    rgba_data.push(green);
-                    rgba_data.push(blue);
-                    rgba_data.push(alpha);
-                }
-
-                // TODO should I premultiply alpha?
-                inner_buf.copy_from_slice(&rgba_data);
+            Background::Image {
+                image: _,
+                resized_image_buffer,
+            } => {
+                buffer.data_mut().copy_from_slice(resized_image_buffer);
             }
+
             Background::Color(color) => {
                 buffer.fill(*color);
             }
@@ -89,7 +82,10 @@ impl TryFrom<&config::Background> for Background {
 
                 Ok(Self::Image {
                     image,
-                    original: None,
+                    // that's an "optimization" that instead of crating the resized_image_buffer from
+                    // the original image and rewriting it after, we just ignore it here so it can
+                    // be populated after. its pretty dumb, but hey, it works.
+                    resized_image_buffer: Vec::new(),
                 })
             }
             config::Background::Color((r, g, b)) => {
